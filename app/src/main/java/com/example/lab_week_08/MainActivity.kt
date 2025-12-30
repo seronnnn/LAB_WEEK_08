0529108ce6f78a6466f8fd6ed012221dc1a61cab
seronnnn <163845781+seronnnn@users.noreply.github.com>
2025-12-31 00:39:16 +0700

    Commit No. 1 – add first & second worker

---
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.lab_week_08
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.work.Constraints
+import androidx.work.Data
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.example.lab_week_08.worker.FirstWorker
+import com.example.lab_week_08.worker.SecondWorker
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var workManager: WorkManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Initialize WorkManager
+        workManager = WorkManager.getInstance(applicationContext)
+
+        val networkConstraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val id = "001"
+
+        val firstRequest = OneTimeWorkRequest
+            .Builder(FirstWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(FirstWorker.INPUT_DATA_ID, id))
+            .build()
+
+        val secondRequest = OneTimeWorkRequest
+            .Builder(SecondWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(FirstWorker.INPUT_DATA_ID, id))
+            .build()
+
+        workManager.beginWith(firstRequest)
+            .then(secondRequest)
+            .enqueue()
+
+        workManager.getWorkInfoByIdLiveData(firstRequest.id)
+            .observe(this) { info ->
+                if (info.state.isFinished) {
+                    showResult("First process is done")
+                }
+            }
+
+        workManager.getWorkInfoByIdLiveData(secondRequest.id)
+            .observe(this) { info ->
+                if (info.state.isFinished) {
+                    showResult("Second process is done")
+                }
+            }
+    }
+
+    private fun getIdInputData(idKey: String, idValue: String) =
+        Data.Builder()
+            .putString(idKey, idValue)
+            .build()
+
+    // Show the result as toast
+    private fun showResult(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
